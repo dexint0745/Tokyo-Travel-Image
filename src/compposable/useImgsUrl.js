8037ea6756c8c9e91765b72bb009a9009a74670a
DESKTOP-CJLTB8M <dexintstr0745@gmail.com>
2025-04-16 16:27:07 +0800

    modify image url

--- src/compposable/useImgsUrl.js
+++ src/compposable/useImgsUrl.js
@@ -6,15 +6,15 @@ function useImgsUrl(data) {
   data_ref = ref(data);
   for (let i = 1; i < 101; i++) {
     if (data_ref.value == "東京市區") {
-      Imgs.push("/src/assets/東京雜圖/東京雜圖_" + i + ".jpg");
+      Imgs.push("./東京雜圖/東京雜圖_" + i + ".jpg");
     } else if (data_ref.value == "富士山") {
-      Imgs.push("/src/assets/富士山雜圖/富士山雜圖_" + i + ".jpg");
+      Imgs.push("./富士山雜圖/富士山雜圖_" + i + ".jpg");
     } else if (data_ref.value == "仙台") {
-      Imgs.push("/src/assets/仙台雜圖/仙台雜圖_" + i + ".jpg");
+      Imgs.push("./仙台雜圖/仙台雜圖_" + i + ".jpg");
     } else {
-      Imgs.push("/src/assets/東京雜圖/東京雜圖_" + i + ".jpg");
-      Imgs.push("/src/assets/富士山雜圖/富士山雜圖_" + i + ".jpg");
-      Imgs.push("/src/assets/仙台雜圖/仙台雜圖_" + i + ".jpg");
+      Imgs.push("./東京雜圖/東京雜圖_" + i + ".jpg");
+      Imgs.push("./富士山雜圖/富士山雜圖_" + i + ".jpg");
+      Imgs.push("./仙台雜圖/仙台雜圖_" + i + ".jpg");
     }
   }
   return Imgs;
